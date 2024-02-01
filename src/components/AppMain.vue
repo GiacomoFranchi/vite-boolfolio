8ee48746e675c4e3641d34808515b56368ba63fc
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-02-01 18:38:57 +0100

    add: AppMain & ProjectCard

---
+++ src/components/AppMain.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
+
+export default{
+    data(){
+        return{
+            Url: 'http://127.0.0.1:8000',
+            projects: []
+        }
+    },
+    created(){
+        axios.get(`${this.Url}/api/projects`).then((resp)=> {
+            this.projects = resp.data.results.data;
+        });
+    },
+    components:{ProjectCard}
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+            <div class="col" v-for="project in projects">
+                <ProjectCard :project="project" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,46 @@
+<script>
+
+export default {
+    data() {
+        return {
+            Url: 'http://127.0.0.1:8000',
+        }
+    },
+    props: {
+        project: Object,
+    },
+    computed: {
+        taglioDescrizione() {
+            if (this.project.content && this.project.content.length > 250) {
+                return this.project.content.substring(0, 150) + "...";
+            }
+            return this.project.content;
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <img :src="`${Url}/storage/${project.img}`" alt="" />
+        <div class="card-body">
+            <h5>{{ project.title }}</h5>
+            <p>
+                <span>Tipologia Progetto:</span>
+                {{ project.type ? project.type.tipologia : "Nessuna Tipologia associata" }}
+            </p>
+            <p>
+                <span>Creato il:</span>
+                {{ project.created_at}}
+            </p>
+            <p><span>Descriozione:</span>:{{ taglioDescrizione }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+span{
+    font-weight: bold;
+    color: red;
+}
+</style>

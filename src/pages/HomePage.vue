74ce749f4fcd47946184e9a3a506448de7928f81
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-02-06 02:12:12 +0100

    add: paginate button prew, next & number page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,13 +7,28 @@ export default{
     data(){
         return{
             store,
-            projects: []
+            projects: [],
+            curPage: 1,
+            lastPage: 1,
+            total: 0
         }
     },
     created(){
-        axios.get(`${this.store.Url}/api/projects`).then((resp)=> {
-            this.projects = resp.data.results.data;
-        });
+        this.getProjects(1);
+    },
+    methods: {
+        getProjects(pageNum){
+            this.curPage = pageNum;
+            axios.get(`${this.store.Url}/api/projects`,{
+                    params:{
+                        page: pageNum,
+                    }
+                }).then((resp)=> {
+                this.projects = resp.data.results.data;
+                this.lastPage = resp.data.results.last_page;
+                this.total = resp.data.results.total;
+            });
+        }
     },
     components:{ProjectCard}
 }
@@ -21,6 +36,12 @@ export default{
 
 <template>
     <div class="container">
+        <div class="my-3">
+            Totali progetti: {{ total }}
+            <button class="btn btn-primary me-2" :disabled="curPage === 1" href="" @click.prevent="getProjects(curPage - 1)">Prev</button>
+            <button class="btn btn-primary me-2" :class="{'btn-success': num === curPage}" v-for="num in lastPage" @click.prevent="getProjects(num)">{{ num }}</button>
+            <button class="btn btn-primary" :disabled="curPage === lastPage" href="" @click.prevent="getProjects(curPage + 1)">Next</button>
+        </div>
         <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
             <div class="col" v-for="project in projects">
                 <ProjectCard :project="project" />
